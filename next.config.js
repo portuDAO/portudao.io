dcbec950afaf51e215ce82a841332076658e758d
MickaelNeves <MickaelNeves@users.noreply.github.com>
2022-03-27 13:11:26 +0000

    deploy: e0d48afe84cff1e1ea9fe440f8b14e81a3da80f0

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? '/web-portudao.com/' : ''
-}
-
-module.exports = nextConfig
