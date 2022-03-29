13e418fe432de381f473b74939365e426714e9be
MickaelNeves <MickaelNeves@users.noreply.github.com>
2022-03-29 14:20:55 +0000

    deploy: 204f03bda9c059e41beece8b055565d93bb684cb

---
+++ _next/static/QyquKyz9TgbSDUeRT2Nb8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/807-07e9c43b8db61134.js","static/chunks/pages/index-1b75a118718263ec.js"],"/_error":["static/chunks/pages/_error-427958541d1e32b7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
