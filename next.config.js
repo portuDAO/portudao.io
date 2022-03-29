d3d17288859771df536b870703d9824d11412fdc
MickaelNeves <nevesmickael8@gmail.com>
2022-03-29 14:38:43 +0100

    Hotfix set img loader to custom for nextjs export

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/portudao.io/' : ''
+  assetPrefix: isProd ? '/portudao.io/' : '',
+  images: { loader: "custom" },
 }
 
 module.exports = nextConfig
